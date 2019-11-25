665e84202e7ab7e670c228af6dc2cb496e114f13
wzf930229 <trace.wang@shijigroup.com>
2019-11-25 16:33:26 +0800

    修改

--- bin/test_run.py
+++ bin/test_run.py
@@ -4,10 +4,10 @@ import HTMLTestRunner
 import time
 if __name__ == '__main__':
     test_dir=os.path.join(os.path.dirname(os.path.dirname(__file__)),"tests")
-    tests=unittest.defaultTestLoader.discover(test_dir,pattern='test*.py')
+    testcase=unittest.defaultTestLoader.discover(test_dir,pattern='test*.py')
     now=time.strftime("%Y-%m-%d_%H_%M_%S",time.localtime(time.time()))
     filepath="d:\\report\\"+now+".html"
     fp=open(filepath,'wb')
     runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title="接口自动化测试报告",description="企业开源框架")
-    runner.run(tests)
+    runner.run(testcase)
     fp.close()
